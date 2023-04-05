faf757f1c2642066cdb84a7d725552ee0449d836
JeffreyXu75 <122924124+JeffreyXu75@users.noreply.github.com>
2023-04-05 00:06:19 -0400

    Finished assignment
    Fulfilled all todos

--- src/models.py
+++ src/models.py
@@ -1,1 +1,23 @@
-# TODO - Create SQLAlchemy DB and Movie model
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
+
+# class Instructor(db.Model):
+#     instructor_id = db.Column(db.Integer, primary_key=True)
+#     first_name = db.Column(db.String(255), nullable=False)
+#     last_name = db.Column(db.String(255), nullable=False)
+#     tenured = db.Column(db.Boolean, nullable=False)
+# -- Create movie table
+# CREATE TABLE movie (
+#     movie_id SERIAL       NOT NULL,
+#     title    VARCHAR(255) NOT NULL,
+#     director VARCHAR(255) NOT NULL,
+#     rating   INT NOT      NULL,
+#     PRIMARY KEY (movie_id)
+# );
+
+class Movie(db.Model):
+     movie_id = db.Column(db.Integer, primary_key=True)
+     title = db.Column(db.String(255), nullable=False)
+     director = db.Column(db.String(255), nullable=False)
+     rating = db.Column(db.Integer, nullable=False)
+
--- src/repositories/movie_repository.py
+++ src/repositories/movie_repository.py
@@ -1,20 +1,29 @@
+from src.models import Movie, db
+from sqlalchemy import func
 class MovieRepository:
+    
 
     def get_all_movies(self):
         # TODO get all movies from the DB
-        return None
+        all = Movie.query.all()
+        return all
 
-    def get_movie_by_id(self, movie_id):
+    def get_movie_by_id(self, movie_id2):
         # TODO get a single movie from the DB using the ID
-        return None
+        x = Movie.query.filter_by(movie_id=movie_id2).first()
+        return x
 
-    def create_movie(self, title, director, rating):
+    def create_movie(self, title2, director2, rating2):
         # TODO create a new movie in the DB
-        return None
+        new_movie = Movie(title=title2, director=director2, rating=rating2)
+        db.session.add(new_movie)
+        db.session.commit()
+        return new_movie
 
     def search_movies(self, title):
         # TODO get all movies matching case insensitive substring (SQL LIKE, use google for how to do with SQLAlchemy)
-        return None
+        x = Movie.query.filter((func.lower(Movie.title).like('%'+title.lower()+'%'))).all()
+        return x
 
 
 # Singleton to be used in other modules
